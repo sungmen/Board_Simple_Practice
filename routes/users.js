5d0848b6bad36cc0ad8823140ffedbb202b7f674
sungmen <spe0678@gmail.com>
2020-06-02 09:54:54 +0900

    edit user.js in show part

--- routes/users.js
+++ routes/users.js
@@ -2,75 +2,75 @@
 
 const express = require('express');
 const router = express.Router();
-let User = require('../models/User');
+const User = require('../models/User');
 
-// Index    
+// Index
 router.get('/', (req, res) => {
-    User.find({})
-        .sort({username:1})
-        .exec((err, users) => {
-            if (err) return res.json(err);
-            res.render('users/index', {users:users});
+  User.find({})
+    .sort({username:1})
+    .exec((err, users) => {
+      if(err) return res.json(err);
+      res.render('users/index', {users:users});
     });
 });
 
 // New
 router.get('/new', (req, res) => {
-    res.render('users/new');
+  res.render('users/new');
 });
 
-// Create
+// create
 router.post('/', (req, res) => {
-    User.create(req.body, (err, user) => {
-        if(err) return res.json(err);
-        res.redirect('/users');
-    });
+  User.create(req.body, (err, user) => {
+    if(err) return res.json(err);
+    res.redirect('/users');
+  });
 });
 
 // show
-router.get('/:usrename', (req, res) => {
-    User.findOne({username:req.params.username}, (err, user) => {
-        if(err) return res.json(err);
-        res.render('users/show', {user:user});
-    });
+router.get('/:username', (req, res)=>{
+  User.findOne({username:req.params.username}, (err, user)=>{
+    if(err) return res.json(err);
+    res.render('users/show', {user:user});
+  });
 });
 
-// Edit
+// edit
 router.get('/:username/edit', (req, res) => {
-    User.findOne({username:req.params.username}, (err, user) => {
-        if (err) return res.json(err);
-        res.render('users/edit', {user:user});
-    });
+  User.findOne({username:req.params.username}, (err, user) => {
+    if(err) return res.json(err);
+    res.render('users/edit', {user:user});
+  });
 });
 
-// update
+// update // 2
 router.put('/:username', (req, res, next) => {
-    User.findOne({username:req.params.username})
-        .select('password')
-        .exec((err, user) => {
-            if (err) return res.json(err);
+  User.findOne({username:req.params.username})
+    .select('password')
+    .exec((err, user) => {
+      if(err) return res.json(err);
 
-            // update user object
-            user.originalPassword = user.password;
-            user.password = req.body.newPassword? req.body.newPassword : user.password;
-            for (let p in req.body) {
-                user[p] = req.body[p];
-            }
+      // update user object
+      user.originalPassword = user.password;
+      user.password = req.body.newPassword? req.body.newPassword : user.password; // 2-3
+      for(var p in req.body){
+        user[p] = req.body[p];
+      }
 
-            // save updated user
-            user.save((err ,user) => {
-                if(err) return res.json(err);
-                res.redirect('/users/' + user.username);
-            });
-    });
+      // save updated user
+      user.save((err, user) => {
+        if(err) return res.json(err);
+        res.redirect('/users/'+user.username);
+      });
+  });
 });
 
 // destroy
 router.delete('/:username', (req, res) => {
-    User.deleteOne({username:req.params.username}, (err) => {
-        if(err) return res.json(err);
-        res.redirect('/users');
-    });
+  User.deleteOne({username:req.params.username}, (err) => {
+    if(err) return res.json(err);
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
